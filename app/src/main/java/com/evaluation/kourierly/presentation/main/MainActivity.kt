0d5b30bc6891d1befe9831ada5f789f67b1c4d95
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-16 23:30:13 +0530

    Add IME padding to the main content.

--- app/src/main/java/com/evaluation/kourierly/presentation/main/MainActivity.kt
+++ app/src/main/java/com/evaluation/kourierly/presentation/main/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
@@ -28,7 +29,8 @@ class MainActivity : ComponentActivity() {
                         modifier =
                             Modifier
                                 .fillMaxSize()
-                                .padding(innerPadding),
+                                .padding(innerPadding)
+                                .imePadding(),
                     )
                 }
             }
